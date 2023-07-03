466d4577f37f2f00783a8c0596ecb0920bf7bd32
Roxane Noël <roxane.noel92@gmail.com>
2023-07-03 09:37:57 +0100

    Deleted more boilerplate + imported Heatmap.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
   <header>
-
+    <h1>Vue Heatmap Component</h1>
   </header>
 
   <main>
-
+    <Heatmap />
   </main>
 </template>
 <script setup lang="ts">
-
+  import Heatmap from './components/Heatmap.vue';
 </script>
 <style scoped>
 
