bace103196b2dd08300c2ba0124f29f9e251b87f
Roxane Noël <roxane.noel92@gmail.com>
2023-07-03 09:39:50 +0100

    Added prop for data

--- src/components/Heatmap.vue
+++ src/components/Heatmap.vue
@@ -4,7 +4,14 @@
 </div>
 </template>
 <script setup lang="ts">
+import { defineProps } from 'vue';
 
+const props = defineProps({
+    data: {
+        type: Object, // Do TS type later once I've created function that formats the data
+        required: true,
+    }
+})
 </script>
 <style scoped lang="scss">
 </style>
